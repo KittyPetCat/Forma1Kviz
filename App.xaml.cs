c5587920a5c479eabb826951e31a7f062cc2efad
KittyPetCat <diaadam07@gmail.com>
2026-01-23 13:25:11 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Forma1Kviz
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
